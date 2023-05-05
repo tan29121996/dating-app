72a60722725d80a7d00098f8e585d9fdfb9e9c0e
tan29121996 <88268203+tan29121996@users.noreply.github.com>
2023-05-05 20:16:20 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHsqTlvbSVRRzClBqw-iN8wdvEtCocdkM",
-  authDomain: "dating-3d3ab.firebaseapp.com",
-  databaseURL: "https://dating-3d3ab.firebaseio.com",
-  projectId: "dating-3d3ab",
-  storageBucket: "dating-3d3ab.appspot.com",
-  messagingSenderId: "677915905036",
-  appId: "1:677915905036:web:ca03d25740b06978d9b229",
-  measurementId: "G-PRW9435QPM"
+
 };
 
 const app = initializeApp(firebaseConfig);
